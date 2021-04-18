a4cad5928851da15e37e3c5f12fcfd6f1d5269a0
Rubens Monteiro <rubensmonteiro1@gmail.com>
2021-04-18 11:16:22 -0300

    Criando e removendo mosquitos

--- JavaScript/App2/jogo.js
+++ JavaScript/App2/jogo.js
@@ -10,6 +10,12 @@ function ajustaTamanhoPalcoJogo() {
 ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
+
+    //remover o mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -25,6 +31,7 @@ function posicaoRandomica() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
 
     document.body.appendChild(mosquito)
 
