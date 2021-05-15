c8ef7b367f99f833ab03caeda235077ca890ecaf
Rubens Monteiro <rubensmonteiro1@gmail.com>
2021-05-15 20:53:05 -0300

    lista despesas part2

--- ES6/app1/app.js
+++ ES6/app1/app.js
@@ -130,5 +130,41 @@ function carregaListaDespesas(){
 
     despesas = bd.recuperarTodosRegistros()
 
-    console.log(despesas)
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    // <tr>
+    //     <td>15/03/2018</td>
+    //     <td>Alimentação</td>
+    //     <td>Compras do mês</td>
+    //     <td>444.75</td>
+    // </tr>
+
+    //percorrer o array despesas, listando cada despesa de forma dinâmica
+    despesas.forEach(function(d){
+
+        console.log(d)
+
+        //criando a linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        //criar as colunas (td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+        //ajustar o tipo
+        switch(d.tipo){
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+        }
+
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
