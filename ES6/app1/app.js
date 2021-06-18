ef71890404e5e2ac841a2daf18e55cafde43a6aa
Rubens Monteiro <rubens.monteiro@multiedro.com.br>
2021-06-17 22:29:01 -0300

    removendo despesas

--- ES6/app1/app.js
+++ ES6/app1/app.js
@@ -61,6 +61,7 @@ class Bd{
                 continue
             }
 
+            despesa.id = i
             despesas.push(despesa)
         }
 
@@ -106,6 +107,10 @@ class Bd{
         }
 
         return despesasFiltradas
+    }
+
+    remover(id){
+        localStorage.removeItem(id)
     }
 }
 
@@ -213,6 +218,23 @@ function carregaListaDespesas(despesas = Array(), filtro = false){
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        //criar o botão de exclusão
+        let btn = document.createElement('button')
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function (){
+            //remover a despesa
+            let id = this.id.replace('id_despesa_','')
+
+            bd.remover(id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
+        console.log(d)
     })
 }
 
