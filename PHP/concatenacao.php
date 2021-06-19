eda8ced2c348f348799ead423bd197a59b30ffc8
Rubens Monteiro <rubens.monteiro@multiedro.com.br>
2021-06-19 14:25:07 -0300

    concatenacao

---
+++ PHP/concatenacao.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Curso PHP</title>
+    </head>
+    <body>
+        <?php
+
+            $nome = 'Maria';
+            $cor = 'amarelo';
+            $idade = 25;
+            $atividade_preferida = 'andar de bicicleta';
+            //operador .
+
+            echo 'Olá '. $nome .', 
+            vi que sua cor preferida é '. $cor .', 
+            estou vendo que você possui '. $idade .' anos e 
+            gosta de jogar '. $atividade_preferida;
+
+            //aspas duplas
+            echo '<br>';
+
+            echo "Olá $nome, 
+            vi que sua cor preferida é $cor, 
+            estou vendo que você possui $idade anos e 
+            gosta de jogar $atividade_preferida";
+
+            echo '<br>';
+
+            echo 'Olá $nome, 
+            vi que sua cor preferida é $cor, 
+            estou vendo que você possui $idade anos e 
+            gosta de jogar $atividade_preferida';
+
+        ?>
+    </body>
+</html>
