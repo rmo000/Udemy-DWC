90e044c796b68f39cb9508099a97e02a15c57929
Rubens Monteiro <rubens.monteiro@multiedro.com.br>
2021-06-19 15:02:04 -0300

    if else e operadores condicionais

---
+++ PHP/ifelse_operadores_condicionais.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Curso PHP</title>
+    </head>
+    <body>
+        <?php
+
+            //==
+            //===
+            //!= ou <>
+            //!==
+            //<
+            //>
+            //<=
+            //>=
+
+            if('a' != 'a'){
+                echo 'Verdadeiro';
+                echo 'Segunda instrução';
+                echo 'Terceira instrução';
+            }
+        ?>
+    </body>
+</html>
