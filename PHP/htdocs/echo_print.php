ba066af20bbb4886d00c5267db756dfa6f666ec5
Rubens Monteiro <rubens.monteiro@multiedro.com.br>
2021-06-19 00:25:15 -0300

    saída de dados echo e print

---
+++ PHP/htdocs/echo_print.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Curso PHP</title>
+    </head>
+    <body>
+        <?php
+            echo 'Comando echo<br>';
+            echo print('Comando print<hr>');
+        ?>
+    </body>
+</html>
